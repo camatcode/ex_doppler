3e2044091c368f73e35eedb2362b2d5f17ab03f2
Cam Cook <cam.cook.codes@gmail.com>
2025-04-25 12:17:35 -0400

    Workplace Roles: List Permissions

--- lib/ex_doppler/workplace_roles.ex
+++ lib/ex_doppler/workplace_roles.ex
@@ -4,10 +4,10 @@ defmodule ExDoppler.WorkplaceRoles do
   alias ExDoppler.Util.Requester
   alias ExDoppler.Workplaces
 
-  def workplace_users_api_path, do: Path.join(Workplaces.workplace_api_path(), "/roles")
+  def workplace_roles_api_path, do: Path.join(Workplaces.workplace_api_path(), "/roles")
 
   def list_workplace_roles() do
-    workplace_users_api_path()
+    workplace_roles_api_path()
     |> Requester.get()
     |> case do
       {:ok, %{body: body}} ->
@@ -20,6 +20,33 @@ defmodule ExDoppler.WorkplaceRoles do
       err ->
         err
     end
+  end
+
+  def get_workplace_role(nil), do: nil
+
+  def get_workplace_role(identifier) do
+    workplace_roles_api_path()
+    |> Path.join("/role/#{identifier}")
+    |> Requester.get()
+    |> case do
+      {:ok, %{body: body}} ->
+        {:ok, build_workplace_role(body["role"])}
+
+      err ->
+        err
+    end
+  end
+
+  def list_permissions() do
+    Path.join(Workplaces.workplace_api_path(), "/permissions")
+    |> Requester.get()
+    |> case do
+         {:ok, %{body: body}} ->
+           {:ok, body["permissions"]}
+
+         err ->
+           err
+       end
   end
 
   defp build_workplace_role(wp_role) do
--- test/ex_doppler/workplace_roles_test.exs
+++ test/ex_doppler/workplace_roles_test.exs
@@ -4,18 +4,26 @@ defmodule ExDoppler.WorkplaceRolesTest do
 
   alias ExDoppler.WorkplaceRoles
 
-  test "list Workplace Roles" do
+  test "Workplace Roles" do
     assert {:ok, roles} = WorkplaceRoles.list_workplace_roles()
     refute Enum.empty?(roles)
+
     roles
     |> Enum.each(fn role ->
-        assert role.created_at
-        assert role.identifier
-        assert role.is_custom_role != nil
-        assert role.is_inline_role != nil
-        assert role.name
-        assert role.permissions
-        refute Enum.empty?(role.permissions)
+      assert role.created_at
+      assert role.identifier
+      assert role.is_custom_role != nil
+      assert role.is_inline_role != nil
+      assert role.name
+      assert role.permissions
+      refute Enum.empty?(role.permissions)
+
+      assert {:ok, role} == WorkplaceRoles.get_workplace_role(role.identifier)
+
+      assert {:err, _} = WorkplaceRoles.get_workplace_role("does-not-exist")
     end)
+
+    assert {:ok, permissions} = WorkplaceRoles.list_permissions()
+    refute Enum.empty?(permissions)
   end
 end
