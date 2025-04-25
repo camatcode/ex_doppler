dbfaa119b01d5886dd3b25b439f5cbe1341f6a39
Cam Cook <cam.cook.codes@gmail.com>
2025-04-25 17:23:45 -0400

    Config Logs: List, Retrieve

---
+++ test/ex_doppler/config_logs_test.exs
@@ -0,0 +1,35 @@
+defmodule ExDoppler.ConfigLogsTest do
+  use ExUnit.Case
+  doctest ExDoppler.ConfigLogs
+
+  alias ExDoppler.ConfigLogs
+  alias ExDoppler.Configs
+
+  test "config logs" do
+    {:ok, %{page: 1, configs: configs}} = Configs.list_configs()
+    refute Enum.empty?(configs)
+
+    configs
+    |> Enum.each(fn config ->
+      {:ok, %{page: 1, logs: logs}} =
+        ConfigLogs.list_config_logs(config.project, config.name)
+
+      refute Enum.empty?(logs)
+
+      logs
+      |> Enum.each(fn log ->
+        assert log.config
+        assert log.created_at
+        assert log.environment
+        assert log.html
+        assert log.id
+        assert log.project
+        assert log.rollback != nil
+        assert log.text
+        assert log.user
+
+        assert {:ok, log} == ConfigLogs.get_config_log(log.project, log.config, log.id)
+      end)
+    end)
+  end
+end
