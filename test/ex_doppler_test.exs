cf889b8c58c67155b31ba37b609ace8f2dfda756
Cam Cook <cam.cook.codes@gmail.com>
2025-05-01 14:19:17 -0400

    README changes

--- test/ex_doppler_test.exs
+++ test/ex_doppler_test.exs
@@ -2,5 +2,35 @@ defmodule ExDopplerTest do
   use ExUnit.Case
   doctest ExDoppler
 
-  # everything is covered by a doctest
+  alias ExDoppler.Configs
+  alias ExDoppler.Projects
+  alias ExDoppler.Secrets
+
+  test "get_secret_raw/3 and get_secret_raw!/3" do
+    assert [project | _] = Projects.list_projects!()
+    assert configs = Configs.list_configs!(project)
+    refute Enum.empty?(configs)
+
+    configs
+    |> Enum.each(fn config ->
+      {:ok, secrets} = Secrets.list_secrets(config)
+      refute Enum.empty?(secrets)
+
+      secrets
+      |> Enum.each(fn secret ->
+        assert secret.name
+        assert secret.raw
+        assert secret.computed
+        assert secret.note
+        assert secret.raw_visibility
+        assert secret.computed_visibility
+
+        {:ok, raw} = ExDoppler.get_secret_raw(project.name, config.name, secret.name)
+        assert raw == secret.raw
+
+        raw = ExDoppler.get_secret_raw!(project.name, config.name, secret.name)
+        assert raw == secret.raw
+      end)
+    end)
+  end
 end
